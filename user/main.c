d17ffb026692575da5cd93006bde6b9fabd42c52
sjsnsnn <2797740343@qq.com>
2026-09-16 22:20:16 +0800

    新增业务逻辑：提取阈值宏定义，实现环境状态判断与串口报警

--- user/main.c
+++ user/main.c
@@ -3,20 +3,39 @@
 #include "OLED.h"   // 假设你已经有了 OLED 驱动
 #include <stdio.h>
 #include "Serial.h"
+
+#define LIGHT_THRESHOLD 1000
   int main(void)
   {
-      OLED_Init();     // 初始化 OLED（如果你有的话）
+      Serial_Init();
+	  OLED_Init();     // 初始化 OLED（如果你有的话）
       AD_Init();          // 初始化 ADC — 开机调一次就够了
-	  Serial_Init();
+	  
+	  OLED_ShowString(1,1,"Light System");
+	  printf("System startup: Light sensor ready...\r\n");
+	  
       while (1)
       {
-          uint16_t light = AD_GetValue(0);  // 读通道 0（PA0），拿到 0~4095
-		  uint16_t brightnes=4095-light;
+          uint16_t light_val = AD_GetValue(0);		  // 读通道 0（PA0），拿到 0~4095
+		  uint16_t brightness=4096-light_val;
+		  uint16_t voltage=brightness*3300/4096;
           // light 值越大 →光越亮
           // light 值越小 →光越暗
-          OLED_ShowNum(1, 1, brightnes, 5);
-
-		  printf("ADC=%d  Brightness=%d\r\n",light ,brightnes);
+          OLED_ShowString(2,1,"Val:");
+		  OLED_ShowNum(2, 5,voltage,4);
+		  OLED_ShowString(2,10,"mV");
+		  
+		  if(brightness<LIGHT_THRESHOLD)
+		  {
+			OLED_ShowString(3,1,"STATUS:DARK");
+			  printf("Warning: Low light, voltage: %d mV\r\n",voltage);
+		  }
+          else
+		  {
+			OLED_ShowString(3,1,"STATUS:BRIGHT");
+			  printf("Normal: Bright, voltage: %d mV\r\n",voltage);
+		  }
+		  
           // 简单延时
           for (uint32_t i = 0; i < 1000000; i++);
       }
